93142a47844c722da534d5e593f41abf575f4105
Elias Barbosa de Sousa <eliasbarbosasousa214@gmail.com>
2023-04-28 01:00:49 -0300

    Ajust

--- src/components/treinamento-components/Banner-T.vue
+++ src/components/treinamento-components/Banner-T.vue
@@ -7,7 +7,7 @@
         <p class="font-bold text-sm  uppercase">Conheça o </p>
         <p class="text-7xl font-bold mb-8"><span class="normal-case underline">Treinamento de Libras</span></p>
         <p class="text-3xl px-20 mb-10 leading-none">da ApraxKids, que é um substituto gratuito para familias e crianças que não tem situação financeira adequada para contratar tratamentos mais avançados.</p>
-        <a href="#sobre" class="transition duration-300 bg-amber-950 bg-opacity-80 hover:bg-opacity-40 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-amber-950">Começar!</a>
+        <a href="#ser-feito" class="transition duration-300 bg-amber-950 bg-opacity-80 hover:bg-opacity-40 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-amber-950">Começar!</a>
         </div>  
     </div>
 </template>
